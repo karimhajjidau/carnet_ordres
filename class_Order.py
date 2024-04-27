ea3b4cbad6d39d98073c12e062ce81056c1aced0
karimhajjidau <162303355+karimhajjidau@users.noreply.github.com>
2024-04-27 16:42:54 +0200

    ajout de commentaires

--- class_Order.py
+++ class_Order.py
@@ -2,9 +2,9 @@ import time
 
 class Order:
     def __init__(self, order_type, side, price, quantity):
-        self.order_id = None
-        self.timestamp = time.time()
-        self.order_type = order_type
-        self.side = side
-        self.price = price
-        self.quantity = quantity
+        self.order_id = None  # ID de l'ordre, initialisé à None
+        self.timestamp = time.time()  # Enregistre le moment de création de l'ordre
+        self.order_type = order_type  # Type d'ordre (par exemple 'limit', 'market')
+        self.side = side  # Côté de l'ordre ('buy' ou 'sell')
+        self.price = price  # Prix à l'unité de l'ordre
+        self.quantity = quantity  # Quantité à acheter ou vendre
